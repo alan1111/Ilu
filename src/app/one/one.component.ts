ae93ce5598ac9e8b3c84afe9543dc0cbc82a47c8
Alan Li <Alan.Li@habatrading.com>
2017-05-31 11:27:59 +0800

    *add Fate turntable

--- src/app/one/one.component.ts
+++ src/app/one/one.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./one.component.css']
 })
 export class OneComponent implements OnInit {
-
+index : number;
+datas: Array<any> = [];
+data: string;
+begin: any;
+ifStop : boolean = false;
   constructor() { }
 
   ngOnInit() {
+    this.datas = ['跪键盘','洗碗涮锅','面壁思过','奖励一盘','做饭去','洗菜','陪看电影','陪逛街','掏腰包','请客','解放一天'];
+    this.interVal();
   }
 
+  interVal() {
+    this.begin = setInterval(() => {
+      this.index = Math.ceil(Math.random()*10);
+      this.data = this.datas[this.index];
+    },100);
+  }
+
+  stop() {
+    this.ifStop = true;
+    clearInterval(this.begin);
+  }
+  start() {
+    this.ifStop = false;
+    this.interVal();
+  }
 }
