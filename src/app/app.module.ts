bd5847adf6a0f8b5759011c80a4e948753e442cb
Alan Li <Alan.Li@habatrading.com>
2017-05-31 10:17:52 +0800

    *cope code from aa project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {OneModule} from "./one/one.module";
+import {AppRoutingModule} from "./app-routing.module";
+import {TwoModule} from "./two/two.module";
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    OneModule,
+    TwoModule,
+    AppRoutingModule,
     HttpModule
   ],
   providers: [],
