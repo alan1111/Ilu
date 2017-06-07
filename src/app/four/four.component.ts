192ddd7558c1462dcac43b23d85793fb685a3243
Alan Li <Alan.Li@habatrading.com>
2017-06-07 16:08:57 +0800

    *release first version.

--- src/app/four/four.component.ts
+++ src/app/four/four.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./four.component.css']
 })
 export class FourComponent implements OnInit {
-
+ num : number = 0;
+ ifShow : boolean = false;
+ data: string;
+ reStart : boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
+  reDo() {
+    this.data = '';
+    this.reStart = false;
+    this.num = 0;
+  }
+  nex() {
+    this.data = '';
+    this.num ++;
+    if (this.num >= 2) {
+      this.reStart = true;
+      this.data = '相当不满意，重做！！！';
+    }
+    this.ifShow = false;
+  }
+  numOne(select : string) {
+    this.ifShow = true;
+    if(select == 'a'){
+      this.data = '哈算聪明，木白跟旋哥混';
+    } else {
+      this.data = '猪一样的队友 ，fu能救你！';
+    }
+  }
 
+  numTwo(select : string) {
+    this.ifShow = true;
+    if(select == 'a'){
+      this.data = '我也勉强答应你吧！';
+    } else {
+      this.data = '可以啊，没人能管你了，是吧？我给你说： 你说咋就咋吧，反正我没意见！';
+    }
+  }
 }
--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-three',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./three.component.css']
 })
 export class ThreeComponent implements OnInit {
-
-  constructor() { }
+  ifhave: boolean = false;
+  data: string;
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.ifhave = true;
+      this.data = '偶哈哈， 你竟然相信上面的话！！！，点我可看更多呦！！';
+    },10000);
   }
-
+  goToNext() {
+    this.router.navigateByUrl('/four');
+  }
 }
